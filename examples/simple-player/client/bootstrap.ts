b10a0c405fdee53861c53ece71cd24a2be91a391
chrisweb <chris963@gmail.com>
2019-06-16 19:31:01 +0200

    fixed import problem

--- examples/simple-player/client/bootstrap.ts
+++ examples/simple-player/client/bootstrap.ts
@@ -2,126 +2,127 @@
 'use strict';
 
 // player build
-import { PlayerCore, PlayerSound } from 'web-audio-api-player/index.esm.js';
+import { PlayerCore, PlayerSound } from '../../../dist/index';
 
 // library
 import { PlayerUI } from './library/player/ui';
 
-/*$(function () {
 
-    let options: ICoreOptions = {
-        soundsBaseUrl: 'https://mp3l.jamendo.com/?trackid=',
-        playingProgressIntervalTime: 500
-    };
 
-    let player = new PlayerCore(options);
-    let playerUI = new PlayerUI(player);
+/*
+let options: ICoreOptions = {
+    soundsBaseUrl: 'https://mp3l.jamendo.com/?trackid=',
+    playingProgressIntervalTime: 500
+};
 
-    let firstSoundAttributes: ISoundAttributes = {
-        sources: '1314412&format=mp31',
-        id: 1314412,
-        playlistId: 0,
-        onLoading: (loadingProgress, maximumValue, currentValue) => {
-            console.log('loading: ', loadingProgress, maximumValue, currentValue);
-            playerUI.setLoadingProgress(loadingProgress);
-        },
-        onPlaying: (playingProgress, maximumValue, currentValue) => {
-            console.log('playing: ', playingProgress, maximumValue, currentValue);
-            playerUI.setPlayingProgress(playingProgress);
-        },
-        onStarted: (playTimeOffset) => {
-            console.log('started', playTimeOffset);
-        },
-        onPaused: (playTimeOffset) => {
-            console.log('paused', playTimeOffset);
-        },
-        onStopped: (playTimeOffset) => {
-            console.log('stopped', playTimeOffset);
-        },
-        onResumed: (playTimeOffset) => {
-            console.log('resumed', playTimeOffset);
-        },
-        onEnded: (willPlayNext) => {
-            console.log('ended', willPlayNext);
-            if (!willPlayNext) {
-                playerUI.switchPlayerContext('on');
-            }
+let player = new PlayerCore(options);
+let playerUI = new PlayerUI(player);
+
+let firstSoundAttributes: ISoundAttributes = {
+    sources: '1314412&format=mp31',
+    id: 1314412,
+    playlistId: 0,
+    onLoading: (loadingProgress, maximumValue, currentValue) => {
+        console.log('loading: ', loadingProgress, maximumValue, currentValue);
+        playerUI.setLoadingProgress(loadingProgress);
+    },
+    onPlaying: (playingProgress, maximumValue, currentValue) => {
+        console.log('playing: ', playingProgress, maximumValue, currentValue);
+        playerUI.setPlayingProgress(playingProgress);
+    },
+    onStarted: (playTimeOffset) => {
+        console.log('started', playTimeOffset);
+    },
+    onPaused: (playTimeOffset) => {
+        console.log('paused', playTimeOffset);
+    },
+    onStopped: (playTimeOffset) => {
+        console.log('stopped', playTimeOffset);
+    },
+    onResumed: (playTimeOffset) => {
+        console.log('resumed', playTimeOffset);
+    },
+    onEnded: (willPlayNext) => {
+        console.log('ended', willPlayNext);
+        if (!willPlayNext) {
+            playerUI.switchPlayerContext('on');
         }
-    };
+    }
+};
 
-    // add the first song to queue
-    let firstSound = player.addSoundToQueue(firstSoundAttributes);
+// add the first song to queue
+let firstSound = player.addSoundToQueue(firstSoundAttributes);
 
-    let secondSoundAttributes: ISoundAttributes = {
-        sources: '1214935&format=ogg1',
-        id: 1214935,
-        playlistId: 0,
-        onLoading: (loadingProgress, maximumValue, currentValue) => {
-            console.log('loading: ', loadingProgress, maximumValue, currentValue);
-            playerUI.setLoadingProgress(loadingProgress);
-        },
-        onPlaying: (playingProgress, maximumValue, currentValue) => {
-            console.log('playing: ', playingProgress, maximumValue, currentValue);
-            playerUI.setPlayingProgress(playingProgress);
-        },
-        onStarted: (playTimeOffset) => {
-            console.log('started', playTimeOffset);
-        },
-        onPaused: (playTimeOffset) => {
-            console.log('paused', playTimeOffset);
-        },
-        onStopped: (playTimeOffset) => {
-            console.log('stopped', playTimeOffset);
-        },
-        onResumed: (playTimeOffset) => {
-            console.log('resumed', playTimeOffset);
-        },
-        onEnded: (willPlayNext) => {
-            console.log('ended', willPlayNext);
-            if (!willPlayNext) {
-                playerUI.switchPlayerContext('on');
-            }
+let secondSoundAttributes: ISoundAttributes = {
+    sources: '1214935&format=ogg1',
+    id: 1214935,
+    playlistId: 0,
+    onLoading: (loadingProgress, maximumValue, currentValue) => {
+        console.log('loading: ', loadingProgress, maximumValue, currentValue);
+        playerUI.setLoadingProgress(loadingProgress);
+    },
+    onPlaying: (playingProgress, maximumValue, currentValue) => {
+        console.log('playing: ', playingProgress, maximumValue, currentValue);
+        playerUI.setPlayingProgress(playingProgress);
+    },
+    onStarted: (playTimeOffset) => {
+        console.log('started', playTimeOffset);
+    },
+    onPaused: (playTimeOffset) => {
+        console.log('paused', playTimeOffset);
+    },
+    onStopped: (playTimeOffset) => {
+        console.log('stopped', playTimeOffset);
+    },
+    onResumed: (playTimeOffset) => {
+        console.log('resumed', playTimeOffset);
+    },
+    onEnded: (willPlayNext) => {
+        console.log('ended', willPlayNext);
+        if (!willPlayNext) {
+            playerUI.switchPlayerContext('on');
         }
-    };
+    }
+};
 
-    // add another song
-    let secondSound = player.addSoundToQueue(secondSoundAttributes);
-
-    
-
-    //let volume = 90;
-
-    //player.setVolume(volume);
-
-    // play first song in the queue
-    //player.play();
-
-    // play next song
-    //player.play(player.PLAY_SOUND_NEXT);
-
-    // TODO: use the sound to display the loading progress
-
-    // TODO: add two sounds, then play the second one by passing it's id, queue should get rid of first song and immediatly play the second one
-
-    // TODO: add a playlist of multiple songs at once, play some song (not the first one), again queue up until that song should get wiped, then play any previous song by id
-    // but as queue won't know that song, we need to trigger some error
-    // do this again but this time reset queue and repopulate it, then play the previous song
-    // TODO: can we add some playlist support to avoid that the user manually needs to manage the queue? especially to avoid having to reset it when playing earlier song and then rebuild himself?
-
-    // TODO: autoplay next song, add two songs, play first one, when ended next one should get played
-
-    // TODO: can we by using the sound, cache its buffer and then re-inject it before playing so that it does get re-loaded/buffered by player
-
-    // TODO: try out playing songs by using play('next / previous / first / last')
-
-    // TODO: add methods to move songs in queue? or remove songs from queue
-
-    // TODO: add history feature and keep track of songs that have been played
-
-    // TODO: can sounds be destroyed? what happens to history if the have been played and get destroyed, what happens if song is in queue or even if song is being played or is buffering when it gets destroyed
-
-    // TODO: test the songs buffer cache, set through options amount of buffers to keep in cache and then check if ajax request is done or not when playing song multiple times
-    // TODO: cache buffer in indexed db or just keep "in memory"?
-
-});
+// add another song
+let secondSound = player.addSoundToQueue(secondSoundAttributes);
 */
+
+
+
+//let volume = 90;
+
+//player.setVolume(volume);
+
+// play first song in the queue
+//player.play();
+
+// play next song
+//player.play(player.PLAY_SOUND_NEXT);
+
+// TODO: use the sound to display the loading progress
+
+// TODO: add two sounds, then play the second one by passing it's id, queue should get rid of first song and immediatly play the second one
+
+// TODO: add a playlist of multiple songs at once, play some song (not the first one), again queue up until that song should get wiped, then play any previous song by id
+// but as queue won't know that song, we need to trigger some error
+// do this again but this time reset queue and repopulate it, then play the previous song
+// TODO: can we add some playlist support to avoid that the user manually needs to manage the queue? especially to avoid having to reset it when playing earlier song and then rebuild himself?
+
+// TODO: autoplay next song, add two songs, play first one, when ended next one should get played
+
+// TODO: can we by using the sound, cache its buffer and then re-inject it before playing so that it does get re-loaded/buffered by player
+
+// TODO: try out playing songs by using play('next / previous / first / last')
+
+// TODO: add methods to move songs in queue? or remove songs from queue
+
+// TODO: add history feature and keep track of songs that have been played
+
+// TODO: can sounds be destroyed? what happens to history if the have been played and get destroyed, what happens if song is in queue or even if song is being played or is buffering when it gets destroyed
+
+// TODO: test the songs buffer cache, set through options amount of buffers to keep in cache and then check if ajax request is done or not when playing song multiple times
+// TODO: cache buffer in indexed db or just keep "in memory"?
+
+
